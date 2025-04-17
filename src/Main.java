c18017f3f55c63133267cc2eab8aff3a31c01384
Karla Castillo <karlacastbg03@gmail.com>
2025-04-17 10:13:31 -0700

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -16,7 +14,7 @@ public class Main {
                 System.out.println("Good enough");
             if (k <=5){
                 System.out.println("Yay");
-            }
+              }
             }
         }
     }
